968282cfb5bf4e1c3f2a5cf0ae45d6704d10ec5a
kkolcz <k.kolcz@outlook.com>
2025-01-28 01:20:55 +0100

    change max password length

--- bam_api/API/DTOs/RegisterDto.cs
+++ bam_api/API/DTOs/RegisterDto.cs
@@ -8,6 +8,6 @@ public class RegisterDto
     public string Username { get; set; } = string.Empty;
     
     [Required]
-    [StringLength(8, MinimumLength = 4)]
+    [StringLength(20, MinimumLength = 4)]
     public string Password { get; set; } = string.Empty;
 }
